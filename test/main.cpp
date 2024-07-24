06d25df8fa8ab0926efe0620dba79af08ffe74c1
basyair7 <basyair.fathul@gmail.com>
2024-07-24 19:34:52 +0700

    first commit project

--- test/main.cpp
+++ test/main.cpp
@@ -2,15 +2,15 @@
 #include "keypad.cpp"
 #include "ds3231rtc.cpp"
 
-KeyTest keytest;
-TestLCD_RTC lcd_rtc;
+KeyTest* keytest = new KeyTest();
+TestLCD_RTC* lcd_rtc = new TestLCD_RTC();
 
 void setup() {
-    // keytest.setup();
-    lcd_rtc.setup();
+    // keytest->setup();
+    lcd_rtc->setup();
 }
 
 void loop() {
-    // keytest.loop();
-    lcd_rtc.loop();
+    // keytest->loop();
+    lcd_rtc->loop();
 }
