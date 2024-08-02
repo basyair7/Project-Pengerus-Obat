8f7845e2eab06714a1a0bc8ee9fe5777a0fee98f
basyair7 <basyair.fathul@gmail.com>
2024-08-02 12:22:14 +0700

    add FreeRTOS system

--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,76 @@
 #include <TypeSerial>
 #include <LCDdisplay>
 #include <DS3231rtc>
-
-LCDdisplay* lcd = new LCDdisplay;
-DS3231rtc* rtc = new DS3231rtc;
+#include <Arduino_FreeRTOS.h>
+#include <task.h>
 
 class ProgramMain {
 public:
-    static void setup() {
-        TSbegin(115200);
-        rtc->begin(nullptr);
-        rtc->autoAdjust(true);
-        lcd->init();
+    void program1(void *pvParameter) {
+        (void) pvParameter;
+        LCDdisplay* lcd = static_cast<LCDdisplay*>(pvParameter);
+        DS3231rtc* rtc = static_cast<DS3231rtc*>(pvParameter);
+        while (true) {
+            lcd->print("Date:" + rtc->datestr(), 0, 0);
+            lcd->print("Time:" + rtc->timestr(), 1, 0);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            lcd->clear();
+        }
     }
 
-    static void loop() {
-        lcd->print("Date:" + rtc->datestr(), 0, 0);
-        lcd->print("Time:" + rtc->timestr(), 1, 0);
-        delay(1000);
-        lcd->clear();
+    void program2(void *pvParameter) {
+        (void) pvParameter;
+        pinMode(LED_BUILTIN, OUTPUT);
+
+        while(true) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            digitalWrite(LED_BUILTIN, LOW);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
     }
 };
 
-// Declared weak in Arduino.h to allow user redefinitions.
-int atexit(void (* /*func*/)()) { return 0; }
+class ThisRTOS : public ProgramMain {
+public:
+    LCDdisplay lcd;
+    DS3231rtc rtc;
 
-// Weak empty variant initialization function.
-void initVariant() __attribute__((weak));
-void initVariant() { }
-
-void setupUSB() __attribute__((weak));
-void setupUSB() { }
-
-int main(void) {
-    init();
-    initVariant();
-
-#if defined(USBCON)
-    USBDevice.attach();
-#endif
-
-    ProgramMain __main__;
-
-    __main__.setup();
-
-    while(1) {
-        __main__.loop();
-        if (serialEventRun) serialEventRun();
+    static ThisRTOS& instance() {
+        static ThisRTOS instance;
+        return instance;
     }
 
-    return 0;
-}
+    static void setup(void) {
+        instance()._setup();
+    }
+
+    static void loop(void) {
+        instance()._loop();
+    }
+
+private:
+    void _setup() {
+        TSbegin(115200);
+        rtc.begin(nullptr);
+        rtc.autoAdjust(true);
+        lcd.init();
+
+        ThisRTOS* p = new ThisRTOS;
+        xTaskCreate([](void* param) {
+            static_cast<ThisRTOS*>(param)->program1(param);
+        }, "program1", 1500, p, 1, NULL);
+        xTaskCreate([](void* param) {
+            static_cast<ThisRTOS*>(param)->program2(param);
+        }, "program2", 1500, p, 1, NULL);
+
+        vTaskStartScheduler();
+    }
+
+    void _loop() {
+        // TODO("Not yet implemented");
+    }
+};
+
+void setup() { ThisRTOS::setup(); }
+void loop() { ThisRTOS::loop(); }
