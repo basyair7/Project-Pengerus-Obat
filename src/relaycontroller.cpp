c82b62349472887b41c024deb3f8384963c608dd
basyair7 <basyair.fathul@gmail.com>
2024-09-06 18:28:25 +0700

    add relaycontroller

---
+++ src/relaycontroller.cpp
@@ -0,0 +1,21 @@
+/*! @file relaycontroller.cpp
+ * @version 1.0.0
+*/
+
+#include <relaycontroller>
+
+relaycontroller::relaycontroller(uint8_t pinRelay) {
+    _pinRelay = pinRelay;
+}
+
+void relaycontroller::init() {
+    pinMode(_pinRelay, OUTPUT);
+}
+
+void relaycontroller::write(uint8_t state, int _delay) {
+    if ((unsigned long) (millis() - LastMillis1) >= (long unsigned int) _delay) {
+        LastMillis1 = millis();
+
+        digitalWrite(_pinRelay, state);
+    }
+}
