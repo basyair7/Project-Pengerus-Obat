1d45744bea7c49ccde6085a39458b47511ddd6c1
basyair7 <basyair.fathul@gmail.com>
2024-09-08 08:15:44 +0700

    add constructor object lcd

--- src/LCDdisplay.cpp
+++ src/LCDdisplay.cpp
@@ -6,24 +6,24 @@
 
 // Initialize LCD program
 void LCDdisplay::init() {
-    lcd->init();
-    lcd->backlight();
-    lcd->clear();
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
 }
 
 // Clear LCD program
 void LCDdisplay::clear() {
-    lcd->clear();
+    lcd.clear();
 }
 
 // Backlight LCD program
 void LCDdisplay::backlight(bool x) {
-    if (x) lcd->backlight();
-    else lcd->noBacklight();
+    if (x) lcd.backlight();
+    else lcd.noBacklight();
 }
 
 // String display program
 void LCDdisplay::print(const String& x, const uint8_t col, const uint8_t row) {
-    lcd->setCursor(col, row);
-    lcd->print(x);
+    lcd.setCursor(col, row);
+    lcd.print(x);
 }
